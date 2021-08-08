06c9e519adf77cf5f1fe9f3fc6f57aa883842fee
Nirushi Wijesiri <niru.pw98@gmail.com>
2021-08-08 15:18:28 +0530

    All files added

---
+++ app/resources/js/main.js
@@ -0,0 +1,8 @@
+// User click on the add button
+// If there is any text inside the field, add it to the todo list
+document.getElementById('add').addEventListener('click', function(){
+    var value = document.getElementById('item').value;
+    if(value){
+        
+    }
+});
